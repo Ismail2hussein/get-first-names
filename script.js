954d6e6633da18af7b0ead454e2081a55b74ccb2
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-09 21:12:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+function getFirstNames(names) {
+   
+    try {
+        if (!Array.isArray(names)) {
+            throw new TypeError("Argument must be an array");
+        }
+        
+        const firstNames = names.map(name => name[0]);
+        return firstNames;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+const names = [["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]];
+const firstNames = getFirstNames(names);
+console.log(firstNames); // Output: ["Ahmed","Jaamac","Mohamed"]
+
